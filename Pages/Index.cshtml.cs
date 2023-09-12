14a4e75ea64d34f1c2b4c2926aa6021ed13b8898
sarvmr <sarv.mr@gmail.com>
2023-09-12 15:16:05 -0700

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace chinesezodiac.Pages;
+
+public class IndexModel : PageModel
+{
+    [BindProperty]
+    public int yearInput { get; set; }
+    public string resultText { get; set; }
+    public string images { get; set; }
+
+    public void OnGet(){
+        //Console.WriteLine(yearInput);
+    }
+
+    public void OnPost(){
+        if (1900 > yearInput || yearInput > 2024){
+            resultText = "Type between 1900 and 2024";
+        }else {
+            resultText = Utils.GetZodiac(yearInput % 12);
+            images = "/images/" + resultText + ".png";
+        }
+    }
+}
